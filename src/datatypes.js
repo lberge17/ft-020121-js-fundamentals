5a204c6ed515d3dd2c8609d5716fc9ebc7873b0f
Laura Berge <lauraeberge@gmail.com>
2021-05-05 09:12:56 -0500

    pre-lecture

---
+++ src/datatypes.js
@@ -0,0 +1,27 @@
+// **** PRIMITIVES (passed by value) ****
+
+// STRING
+
+// NUMBER
+
+// BOOLEAN
+
+// UNDEFINED
+
+// NULL
+
+// SYMBOL (we won't really use these during the program)
+// extra: https://www.youtube.com/watch?v=4J5hnOCj69w
+
+// **** NON-PRIMITIVES (passed by reference) ****
+
+// ARRAY
+
+// OBJECT (object literals / Plain Old JavaScript Object)
+
+// FUNCTION
+// closures
+
+// function expression
+
+// function declarations
